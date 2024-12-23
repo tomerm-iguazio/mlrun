8e53e36b73bb9e80441e85c9e6b259eba17cccff
Tomer Mamia <tomerm@iguazio.com>
2024-12-23 13:48:10 +0200

    merge all functions

--- server/py/services/api/api/endpoints/model_monitoring.py
+++ server/py/services/api/api/endpoints/model_monitoring.py
@@ -115,6 +115,7 @@ async def _common_parameters(
     "is deprecated in 1.8.0 and will be removed in 1.10.0, "
     "use PUT /projects/{project}/model-monitoring/ instead",
 )
+@router.put("/")
 async def enable_model_monitoring(
     commons: Annotated[_CommonParams, Depends(_common_parameters)],
     base_period: int = 10,
@@ -165,51 +166,8 @@ async def enable_model_monitoring(
     "is deprecated in 1.8.0 and will be removed in 1.10.0, "
     "use /projects/{project}/model-monitoring/controller instead",
 )
-async def update_model_monitoring_controller(
-    commons: Annotated[_CommonParams, Depends(_common_parameters)],
-    base_period: int = 10,
-    image: str = "mlrun/mlrun",
-):
-    """
-    Redeploy model monitoring application controller function.
-    The main goal of the controller function is to handle the monitoring processing and triggering applications.
-
-    :param commons:     The common parameters of the request.
-    :param base_period: The time period in minutes in which the model monitoring controller function
-                        triggers. By default, the base period is 10 minutes.
-    :param image:       The default image of the model monitoring controller job. Note that the writer
-                        function, which is a real time nuclio functino, will be deployed with the same
-                        image. By default, the image is mlrun/mlrun.
-    """
-    try:
-        # validate that the model monitoring stream has not yet been deployed
-        mlrun.runtimes.nuclio.function.get_nuclio_deploy_status(
-            name=mlrun.common.schemas.model_monitoring.MonitoringFunctionNames.APPLICATION_CONTROLLER,
-            project=commons.project,
-            tag="",
-            auth_info=commons.auth_info,
-        )
-
-    except mlrun.errors.MLRunNotFoundError:
-        raise mlrun.errors.MLRunNotFoundError(
-            f"{mlrun.common.schemas.model_monitoring.MonitoringFunctionNames.APPLICATION_CONTROLLER} does not exist. "
-            f"Run `project.enable_model_monitoring()` first."
-        )
-
-    return MonitoringDeployment(
-        project=commons.project,
-        auth_info=commons.auth_info,
-        db_session=commons.db_session,
-        model_monitoring_access_key=commons.model_monitoring_access_key,
-    ).deploy_model_monitoring_controller(
-        controller_image=image,
-        base_period=base_period,
-        overwrite=True,
-    )
-
-
 @router.patch("/controller")
-async def update_model_monitoring_controller_v2(
+async def update_model_monitoring_controller(
     commons: Annotated[_CommonParams, Depends(_common_parameters)],
     base_period: int = 10,
     image: str = "mlrun/mlrun",
@@ -260,26 +218,8 @@ async def update_model_monitoring_controller_v2(
     "is deprecated in 1.8.0 and will be removed in 1.10.0, "
     "use PUT /projects/{project}/model-monitoring/histogram-data-drift-app instead",
 )
-def deploy_histogram_data_drift_app(
-    commons: Annotated[_CommonParams, Depends(_common_parameters)],
-    image: str = "mlrun/mlrun",
-) -> None:
-    """
-    Deploy the histogram data drift app on the go.
-
-    :param commons: The common parameters of the request.
-    :param image:   The image of the application, defaults to "mlrun/mlrun".
-    """
-    MonitoringDeployment(
-        project=commons.project,
-        auth_info=commons.auth_info,
-        db_session=commons.db_session,
-        model_monitoring_access_key=commons.model_monitoring_access_key,
-    ).deploy_histogram_data_drift_app(image=image)
-
-
 @router.put("/histogram-data-drift-app")
-def deploy_histogram_data_drift_app_v2(
+def deploy_histogram_data_drift_app(
     commons: Annotated[_CommonParams, Depends(_common_parameters)],
     image: str = "mlrun/mlrun",
 ) -> None:
@@ -309,6 +249,14 @@ def deploy_histogram_data_drift_app_v2(
     description="/projects/{project}/model-monitoring/disable-model-monitoring "
     "is deprecated in 1.8.0 and will be removed in 1.10.0, "
     "use DELETE /projects/{project}/model-monitoring/ instead",
+)
+@router.delete(
+    "/",
+    responses={
+        http.HTTPStatus.ACCEPTED.value: {
+            "model": mlrun.common.schemas.BackgroundTaskList
+        },
+    },
 )
 async def disable_model_monitoring(
     commons: Annotated[_CommonParams, Depends(_common_parameters)],
@@ -408,6 +356,7 @@ async def delete_model_monitoring_function(
     "is deprecated in 1.8.0 and will be removed in 1.10.0, "
     "use PUT /projects/{project}/model-monitoring/credentials instead",
 )
+@router.put("/credentials")
 def set_model_monitoring_credentials(
     commons: Annotated[_CommonParams, Depends(_common_parameters)],
     access_key: Optional[str] = None,
@@ -448,148 +397,3 @@ def set_model_monitoring_credentials(
         tsdb_connection=tsdb_connection,
         replace_creds=replace_creds,
     )
-
-
-@router.put("/credentials")
-def set_model_monitoring_credentials_v2(
-    commons: Annotated[_CommonParams, Depends(_common_parameters)],
-    access_key: Optional[str] = None,
-    stream_path: Optional[str] = None,
-    tsdb_connection: Optional[str] = None,
-    replace_creds: bool = False,
-) -> None:
-    """
-    Set the credentials that will be used by the project's model monitoring
-    infrastructure functions. Important to note that you have to set the credentials before deploying any
-    model monitoring or serving function.
-    :param commons:                   The common parameters of the request.
-    :param access_key:                Model Monitoring access key for managing user permissions.
-    :param stream_path:               Path to the model monitoring stream. By default, None.
-                                      Options:
-                                      1. None, will be set from the system configuration.
-                                      2. v3io - for v3io stream,
-                                         pass `v3io` and the system will generate the exact path.
-                                      3. Kafka - for Kafka stream, please provide full connection string without
-                                         custom topic, for example kafka://<some_kafka_broker>:<port>.
-    :param tsdb_connection:           Connection string to the time series database. By default, None.
-                                      Options:
-                                      1. None, will be set from the system configuration.
-                                      2. v3io - for v3io stream,
-                                         pass `v3io` and the system will generate the exact path.
-                                      3. TDEngine - for TDEngine tsdb, please provide full websocket connection URL,
-                                         for example taosws://<username>:<password>@<host>:<port>.
-    :param replace_creds:             If True, it will force the credentials update. By default, False.
-    """
-    MonitoringDeployment(
-        project=commons.project,
-        auth_info=commons.auth_info,
-        db_session=commons.db_session,
-        model_monitoring_access_key=commons.model_monitoring_access_key,
-    ).set_credentials(
-        access_key=access_key,
-        stream_path=stream_path,
-        tsdb_connection=tsdb_connection,
-        replace_creds=replace_creds,
-    )
-
-
-@router.put("/")
-async def enable_model_monitoring_v2(
-    commons: Annotated[_CommonParams, Depends(_common_parameters)],
-    base_period: int = 10,
-    image: str = "mlrun/mlrun",
-    deploy_histogram_data_drift_app: bool = True,
-    rebuild_images: bool = False,
-    fetch_credentials_from_sys_config: bool = False,
-):
-    """
-    Deploy model monitoring application controller, writer and stream functions.
-    While the main goal of the controller function is to handle the monitoring processing and triggering
-    applications, the goal of the model monitoring writer function is to write all the monitoring
-    application results to the databases.
-    And the stream function goal is to monitor the log of the data stream. It is triggered when a new log entry
-    is detected. It processes the new events into statistics that are then written to statistics databases.
-
-    :param commons:                           The common parameters of the request.
-    :param base_period:                       The time period in minutes in which the model monitoring controller
-                                              function triggers. By default, the base period is 10 minutes.
-    :param image:                             The image of the model monitoring controller, writer & monitoring
-                                              stream functions, which are real time nuclio functions.
-                                              By default, the image is mlrun/mlrun.
-    :param deploy_histogram_data_drift_app:   If true, deploy the default histogram-based data drift application.
-    :param rebuild_images:                    If true, force rebuild of model monitoring infrastructure images
-                                              (controller, writer & stream).
-    :param fetch_credentials_from_sys_config: If true, fetch the credentials from the system configuration.
-
-    """
-    MonitoringDeployment(
-        project=commons.project,
-        auth_info=commons.auth_info,
-        db_session=commons.db_session,
-        model_monitoring_access_key=commons.model_monitoring_access_key,
-    ).deploy_monitoring_functions(
-        image=image,
-        base_period=base_period,
-        deploy_histogram_data_drift_app=deploy_histogram_data_drift_app,
-        rebuild_images=rebuild_images,
-        fetch_credentials_from_sys_config=fetch_credentials_from_sys_config,
-    )
-
-
-@router.delete(
-    "/",
-    responses={
-        http.HTTPStatus.ACCEPTED.value: {
-            "model": mlrun.common.schemas.BackgroundTaskList
-        },
-    },
-)
-async def disable_model_monitoring_v2(
-    commons: Annotated[_CommonParams, Depends(_common_parameters)],
-    background_tasks: fastapi.BackgroundTasks,
-    response: fastapi.Response,
-    delete_resources: bool = True,
-    delete_stream_function: bool = False,
-    delete_histogram_data_drift_app: bool = True,
-    delete_user_applications: bool = False,
-    user_application_list: Optional[list[str]] = None,
-):
-    """
-    Disable model monitoring application controller, writer, stream, histogram data drift application
-    and the user's applications functions, according to the given params.
-
-    :param commons:                             The common parameters of the request.
-    :param background_tasks:                    Background tasks.
-    :param response:                            The response.
-    :param delete_resources:                    If True, it would delete the model monitoring controller & writer
-                                                functions. Default True
-    :param delete_stream_function:              If True, it would delete model monitoring stream function,
-                                                need to use wisely because if you're deleting this function
-                                                this can cause data loss in case you will want to
-                                                enable the model monitoring capability to the project.
-                                                Default False.
-    :param delete_histogram_data_drift_app:     If True, it would delete the default histogram-based data drift
-                                                application. Default False.
-    :param delete_user_applications:            If True, it would delete the user's model monitoring
-                                                application according to user_application_list, Default False.
-    :param user_application_list:               List of the user's model monitoring application to disable.
-                                                Default all the applications.
-                                                Note: you have to set delete_user_applications to True
-                                                in order to delete the desired application.
-
-    """
-    tasks = await MonitoringDeployment(
-        project=commons.project,
-        auth_info=commons.auth_info,
-        db_session=commons.db_session,
-        model_monitoring_access_key=commons.model_monitoring_access_key,
-    ).disable_model_monitoring(
-        delete_resources=delete_resources,
-        delete_stream_function=delete_stream_function,
-        delete_histogram_data_drift_app=delete_histogram_data_drift_app,
-        delete_user_applications=delete_user_applications,
-        user_application_list=user_application_list,
-        background_tasks=background_tasks,
-    )
-    response.status_code = http.HTTPStatus.ACCEPTED.value
-    return tasks
