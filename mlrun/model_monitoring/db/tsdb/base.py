c9d076dbea5cf8fa06a62983796fc0bdc96d747a
Tomer Mamia <tomerm@iguazio.com>
2024-12-29 13:56:14 +0200

    fix comment

--- mlrun/model_monitoring/db/tsdb/base.py
+++ mlrun/model_monitoring/db/tsdb/base.py
@@ -510,7 +510,7 @@ class TSDBConnector(ABC):
             name_column = mm_schemas.MetricData.METRIC_NAME
 
         grouped_by_fields.append(name_column)
-        #  groupby has different behavior for category columns
+        # groupby has different behavior for category columns
         df["endpoint_id"] = df["endpoint_id"].astype(str)
         grouped_by_df = df.groupby("endpoint_id")
         grouped_dict = grouped_by_df.apply(
@@ -568,7 +568,7 @@ class TSDBConnector(ABC):
             name_column = mm_schemas.MetricData.METRIC_NAME
         columns_to_zip.insert(1, name_column)
 
-        #  groupby has different behavior for category columns
+        # groupby has different behavior for category columns
         df["endpoint_id"] = df["endpoint_id"].astype(str)
         df["event_values"] = list(zip(*[df[col] for col in columns_to_zip]))
         grouped_by_event_values = df.groupby("endpoint_id")["event_values"].apply(set)
