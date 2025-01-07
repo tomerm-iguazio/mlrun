040ecc76fac3ab4b7e0000a88023eeed5b5ebce4
alxtkr77 <3098237+alxtkr77@users.noreply.github.com>
2025-01-07 16:31:21 +0200

    [Datastore] Fix vectordb tests and default params in log_document() and move the `collections` metadata for document artifacts to the status (#7007)

--- mlrun/projects/project.py
+++ mlrun/projects/project.py
@@ -1965,6 +1965,7 @@ class MlrunProject(ModelObj):
             ... )
 
         """
+        document_loader_spec = document_loader_spec or DocumentLoaderSpec()
         if not document_loader_spec.download_object and upload:
             raise ValueError(
                 "This document loader expects direct links/URLs and does not support file uploads. "
@@ -1973,9 +1974,8 @@ class MlrunProject(ModelObj):
         doc_artifact = DocumentArtifact(
             key=key,
             original_source=local_path or target_path,
-            document_loader_spec=document_loader_spec
-            if document_loader_spec
-            else DocumentLoaderSpec(),
+            document_loader_spec=document_loader_spec,
+            collections=kwargs.pop("collections", None),
             **kwargs,
         )
         return self.log_artifact(
