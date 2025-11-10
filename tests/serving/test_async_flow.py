7b882203e3ddb541001c278b1c239394bcca9c32
Tomer Mamia <tomerm@iguazio.com>
2025-11-10 17:04:32 +0200

    start working on BatchedModel

--- tests/serving/test_async_flow.py
+++ tests/serving/test_async_flow.py
@@ -244,6 +244,23 @@ class MyModel(Model):
         return self.predict(event)
 
 
+class BatchedModel(Model):
+    def __init__(self, multi: int, **kwargs):
+        super().__init__(**kwargs)
+        self.multi = multi
+
+    def predict(self, body):
+        result = [x * self.multi for x in body["n"]]
+        return result
+
+    async def predict_async(self, body):
+        result = [x * self.multi for x in body["n"]]
+        return result
+
+    def do(self, event):
+        return self.predict(event)
+
+
 class MyRemoteModel(Model):
     def predict(self, body, **kwargs):
         body["url"] = self.model_artifact.model_url
@@ -1296,3 +1313,36 @@ def test_configure_model_runner_step_max_threads_processes(concurrency: str):
         ), "Max threads not configured properly"
     server.test(body={"n": 1})
     server.wait_for_completion()
+
+
+def test_mrs_batch():
+    function = mlrun.new_function("tests", kind="serving")
+    graph = function.set_topology("flow", engine="async")
+    model_runner_step = ModelRunnerStep(name="my_model_runner")
+    model_runner_step.add_model(
+        model_class="BatchedModel",
+        execution_mechanism="naive",
+        endpoint_name="my_model_1",
+        inc=1,
+    )
+    model_runner_step.add_model(
+        model_class="BatchedModel",
+        execution_mechanism="naive",
+        endpoint_name="my_model_2",
+        inc=2,
+    )
+    graph.to(
+        name="echo",
+        class_name="Echo",
+        model_endpoint_creation_strategy=schemas.ModelEndpointCreationStrategy.SKIP,
+    ).to(model_runner_step).respond()
+    assert set(graph.model_endpoints_names) == {
+        "my_model_1",
+        "my_model_2",
+    }, "model endpoints name not in graph"
+    server = function.to_mock_server()
+    try:
+        resp = server.test(body={"n": [1, 2, 3]})
+        assert resp == {"my_model_1": {"n": 2}, "my_model_2": {"n": 3}}
+    finally:
+        server.wait_for_completion()
