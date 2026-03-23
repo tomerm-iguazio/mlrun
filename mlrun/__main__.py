9b7b8d6970756db618f00d8d5c45f7bf1f65d016
Tomer_Mamia <tomerm@iguazio.com>
2026-03-23 13:00:45 +0200

    fix notification file opening

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -1492,8 +1492,8 @@ def load_notification(notifications: str, project: mlrun.projects.MlrunProject):
     for notification in notifications:
         if notification.startswith("file="):
             file_path = notification.split("=")[-1]
-            notification = open(file_path)
-            notification = json.load(notification)
+            with open(file_path, "r") as notification_file:
+                notification = json.load(notification_file)
         else:
             notification = json.loads(notification)
         add_notification_to_project(notification, project)
