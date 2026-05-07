ecee1351af909533b8c9935c5acd5c0de7add278
Tomer_Mamia <tomerm@iguazio.com>
2026-05-07 12:51:34 +0300

    add sleep

--- mlrun/datastore/model_provider/huggingface_provider.py
+++ mlrun/datastore/model_provider/huggingface_provider.py
@@ -121,11 +121,14 @@ class HuggingFaceProvider(ModelProvider):
             from huggingface_hub import snapshot_download
 
             # Download the model and tokenizer files directly to the cache.
-            snapshot_download(
-                repo_id=self.model,
-                local_dir_use_symlinks=False,
-                token=self._get_secret_or_env("HF_TOKEN") or None,
-            )
+            print("sleeping")
+            import time
+            time.sleep(1000)
+            # snapshot_download(
+            #     repo_id=self.model,
+            #     local_dir_use_symlinks=False,
+            #     token=self._get_secret_or_env("HF_TOKEN") or None,
+            # )
         except ImportError as exc:
             raise ImportError("huggingface_hub package is not installed") from exc
 
